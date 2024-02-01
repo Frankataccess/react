88e7ca345b1b552a91b78c7098fc9ec360fb3d3a
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2024-02-01 10:28:19 +0000

    init

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  let name = "Frank";
+  let name = "DAVE";
+
+  const handleclick = () => {
+    alert("Hello " + name);
+  }
 
   return (
    <> 
-     <h1>Hello {name} </h1>
+   <Navbar/>
+     <h3>MY NAME A {name} </h3>
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam magnam commodi animi libero, dolor cumque ut fugiat dolorem ex ab esse excepturi quo beatae exercitationem non corporis vel veniam quos!</p>
-   </> 
+
+     <button onClick={() => handleclick ("DAVE")}>Click me</button>
+   </>
    )
 }
 
-export default App
+export default App;
---
+++ vite-project/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <header className="bg-blue-900 gap-3 px-10">
+            
+            <h1 className="grow text-3xl font-bold">Website</h1>
+            <a href="/">Twitter</a>
+            <a href="/">Linkedin</a>
+            <a href="/">Github</a>
+        </header>
+    );
+}
+ 
+export default Navbar;
